fa9c27825ec8ec62003b59bd30a7d475e724dc99
Damian Rojek <draxan44@gmail.com>
2024-10-04 16:30:19 +0200

    Zmiana nazwy pliku z main na Zyczenia_urodzinowe

--- Życzenia_urodzinowe.py
+++ Życzenia_urodzinowe.py
@@ -15,7 +15,8 @@ wiek = datetime.now().year - rok
 
 separator=("-"*100)
 
-szablon_urodzinowy ="""W tym wyjątkowym dniu, życzę Ci, aby każdy kolejny dzień przynosił Ci wiele radości i spełnienia. 
+szablon_urodzinowy ="""
+W tym wyjątkowym dniu, życzę Ci, aby każdy kolejny dzień przynosił Ci wiele radości i spełnienia. 
 Niech Twoje marzenia, nawet te najskrytsze, stają się rzeczywistością. 
 Niech każdy krok na Twojej drodze będzie pełen sukcesów, a otaczający Cię ludzie niech zawsze wspierają Cię w Twoich dążeniach. 
 Dużo zdrowia, uśmiechu i niekończącej się energii do podejmowania nowych wyzwań!\n\nWszystkiego najlepszego!"""
