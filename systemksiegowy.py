4b0de6c8663b3763a14c6634441bda627748df67
Damian Rojek <draxan44@gmail.com>
2024-10-19 19:46:53 +0200

    dodanie mozliwosci zakupu samochodu etc. - spora modyfikacja kodu

--- systemksiegowy.py
+++ systemksiegowy.py
@@ -27,13 +27,45 @@ autozbior = [
 while True:
     print("Witam w programie do sprzedaży samochodów")
     wybor_użytkownika = input("Wybierz funkcje programu\n"
-                              "1.Zmiana salda firmy\n"
-                              "2.Sprzedaż samochodu\n"
-                              "3.Dodanie samochodu do floty\n"
-                              "4.Wyswietl stan konta firmy\n"
-                              "5.Lista dostępnych pojazdów\n"
-                              "6.Wyswietl dostepnośc konkretnego samochodu\n"
-                              "7.Przeglad działań\n"
-                              "8.Koniec")
+                              "1. Zmiana salda firmy\n"
+                              "2. Sprzedaż samochodu\n"
+                              "3. Dodanie samochodu do floty\n"
+                              "4. Wyswietl stan konta firmy\n"
+                              "5. Lista dostępnych pojazdów\n"
+                              "6. Wyswietl dostepnośc konkretnego samochodu\n"
+                              "7. Przeglad działań\n"
+                              "8. Koniec\n"
+                              "Wybór działania : ")
+    if wybor_użytkownika == "1":
+        kwota = float(input("Podaj kwotę o jaką chcesz zwiekszyć/zmniejszyć saldo: "))
+        if Saldo + kwota <0:
+            print("Brak środków na koncie !")
+            kontener.append("Próba odjecia zbyt dużej kwoty")
+        else:
+            Saldo += kwota
+            kontener.append(f"Zmiana salda o {kwota}")
+
+    elif wybor_użytkownika == "2":
+        marka = input("Podaj marke którą chcesz zakupić: ")
+        model = input("Podaj model który chcesz zakupić: ")
+        rok = input("Podaj rok wyprodukowania: ")
+        znaleziono_samochod = False
+        for samochod in autozbior:
+            if samochod.get("marka") == marka and samochod.get("model") == model and samochod.get("rok") == rok:
+                if samochod.get("ilość_dostepnych_sztuk") >= 1:
+                    samochod["ilość_dostepnych_sztuk"] -=1
+                    Saldo += samochod.get("cena")
+                else:
+                    print("Ten model został całkowicie wyprzedany, dostawa w trakcie")
+                znaleziono_samochod = True
+                break
+        if not znaleziono_samochod:
+            print("Nie znaleziono samochodu\n")
+
+    elif wybor_użytkownika == "3":
+
+    elif wybor_użytkownika == "4":
+        print(Saldo)
+
     if wybor_użytkownika == "8":
         break
