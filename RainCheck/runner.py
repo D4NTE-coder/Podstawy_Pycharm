d9d610bdb67c3ab232013d563146ac79e8fc3c52
D4NTE-coder <draxan44@gmail.com>
2024-12-15 20:15:12 +0100

    dodanie podstawowych funkcjonalnosci programu

--- RainCheck/runner.py
+++ RainCheck/runner.py
@@ -4,4 +4,4 @@ import datetime
 def main():
     latitude = input("Podaj szerokość geograficzna (latitude): ")
     longitude = input("Podaj długość geograficzna (longitude): ")
-    latitude = input("Podaj szerokość geograficzna (latitude): ")
+    date_input = input("Podaj date w formacie YYYY-MM-DD (domyślnie jutrzejsza data): ")
