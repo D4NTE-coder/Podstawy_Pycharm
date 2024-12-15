44c599de27b112b294ad4a14f57f79393585224a
D4NTE-coder <draxan44@gmail.com>
2024-12-15 20:14:11 +0100

    dodanie podstawowych funkcjonalnosci programu

---
+++ RainCheck/runner.py
@@ -0,0 +1,7 @@
+import datetime
+
+
+def main():
+    latitude = input("Podaj szerokość geograficzna (latitude): ")
+    longitude = input("Podaj długość geograficzna (longitude): ")
+    latitude = input("Podaj szerokość geograficzna (latitude): ")
