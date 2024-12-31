9ededc7b89b601e0d429ed4f1153620980929c66
D4NTE-coder <draxan44@gmail.com>
2024-12-31 15:19:10 +0100

    dodanie mozliwosci dodania kilku dat

--- RainCheck_version2/runner.py
+++ RainCheck_version2/runner.py
@@ -19,6 +19,29 @@ def main():
                 print(f"Nie znaleziono koordynatów dla {city_name}")
             print(f"Współrzędne dla miasta {city_name}: {coordinates[0]}, {coordinates[1]}")
 
+            date_input = input("Podaj daty w formacie YYYY-MM-DD oddzielone przecinkami (domyślnie jutrzejsza data): ")
+            if not date_input:
+                dates = [(datetime.date.today() + datetime.timedelta(days=1)).strftime("%Y-%m-%d")]
+            else:
+                dates = [date.strip() for date in date_input.strip(",")]
+
+                for i, date in enumerate(dates):
+                    try:
+                        datetime.datetime.strptime(date, "%Y-%m-%d")
+                    except ValueError:
+                        print(f"Niepoprawny format daty: {date}. Używam domyślnej daty")
+                        dates[i] = (datetime.date.today() + datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+
+            for searched_date in dates:
+                if searched_date in forecast:
+                    print(f"Wynik dla {city_name} ({searched_date})(z pliku): {forecast[searched_date]}")
+                else:
+                    result = check_rain(coordinates[0],[coordinates[1]], searched_date)
+                    print(f"Wynik dla {city_name} ({searched_date}): {result}")
+                    result_data = {searched_date: {"city": city_name,"rain prediction": result}}
+                    forecast.save(result_data)
+
+
 
 
 if __name__ == "__main__":
