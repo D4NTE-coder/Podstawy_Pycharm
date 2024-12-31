45dda89a2d3cda53eaf8fd529c6fd8d5da0a0d80
D4NTE-coder <draxan44@gmail.com>
2024-12-31 14:50:35 +0100

    Dodanie 2 wersji - dodanie mozliwosci wpisania kilku miast

---
+++ RainCheck_version2/runner.py
@@ -0,0 +1,24 @@
+import datetime
+from RainCheck_version2.file_handler import WeatherForecast
+from utils import check_rain, get_coordinates
+
+
+def main():
+    forecast = WeatherForecast
+    while True:
+
+        cities_name = input("Podaj nazwe miast: (oddzielone przecinkami, lub wpisze 'exit' aby zakonczyć")
+        if cities_name.lower() == "exit":
+            break
+
+        cities = [city.strip() for city in cities_name.split(",")]
+
+        for city_name in cities:
+            coordinates = get_coordinates(city_name)
+            if not coordinates:
+                print(f"Nie znaleziono koordynatów dla {city_name}")
+            print(f"Współrzędne dla miasta {city_name}: {coordinates[0]}, {coordinates[1]}")
+
+
+
+
---
+++ RainCheck_version2/utils.py
@@ -0,0 +1,28 @@
+import requests
+from geopy.geocoders import Nominatim
+
+
+def get_coordinates(city_name):
+    geolocator = Nominatim(user_agent="weather_app")
+    location = geolocator.geocode(city_name)
+    if location:
+        return location.latitude, location.longitude
+    else:
+        return None
+
+
+def check_rain(latitude, longitude, searched_date):
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=rain&daily=rain_sum&timezone=Europe%2FLondon&start_date={searched_date}&end_date={searched_date}"
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        rain_sum = data.get("daily", {}).get("rain_sum", [None])[0]
+        if rain_sum is None or rain_sum < 0:
+            return "Nie wiem"
+        elif rain_sum > 0.0:
+            return "Będzie padać"
+        else:
+            return "Nie bedzie padać"
+    else:
+        return "Nie wiem"
